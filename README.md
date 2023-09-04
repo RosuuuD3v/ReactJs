d8d1b56bc4da24a195c01afbd43f60b95e9aa3a8
james <jsbibjames@itcom888.com>
2023-09-04 13:40:57 +0800

    intial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactJs
-React Js portfolio
---
+++ travel-list/src/App.js
@@ -0,0 +1,56 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
